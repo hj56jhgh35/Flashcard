f037f09ffd9d68e51b5995b2fa54643b68263c2d
pablo siqueira raminelli <00001081445099sp@al.educacao.sp.gov.br>
2024-11-08 14:10:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem foi o autor de "Dom Quixote", uma das maiores obras da literatura espanhola?',
+    'Miguel de Cervantes.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é a fotossíntese?',
+    'A fotossíntese é o processo pelo qual as plantas produzem seu alimento a partir da luz solar, dióxido de carbono e água, liberando oxigênio como subproduto.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Japão?',
+    ' Tóquio.'
+)
+
+criaCartao(
+    'História da arte',
+    'Quem pintou a obra "A Última Ceia"?',
+    'Leonardo da vinci'
+)
